9b9916939ff750142680eddeb4e52243edd9f47e
Riddhi <rxiddhi@gmail.com>
2025-06-20 00:27:34 +0530

    Protect Routes Using PrivateRoute & JWT Authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import Navbar from './components/Navbar';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NotesUploadPage from './components/NotesUploadPage';
 import NotesBrowsingPage from './components/NotesBrowsingPage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/upload" element={<NotesUploadPage />} />
-        <Route path="/browse" element={<NotesBrowsingPage />} />
+        
+        {/* Protected Routes */}
+        <Route 
+          path="/upload" 
+          element={
+            <PrivateRoute>
+              <NotesUploadPage />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/browse" 
+          element={
+            <PrivateRoute>
+              <NotesBrowsingPage />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
